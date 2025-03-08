4b115ca1a1210d0efc18566f465c9877c4193815
expband <maxteroidyandex@gmail.com>
2025-03-08 16:28:46 +0100

    postback process fix

--- app/services/crm_postback_service.py
+++ app/services/crm_postback_service.py
@@ -8,35 +8,36 @@ class CrmPostbackService:
         self.__dilovod_client = dilovod_client
 
     async def process_postback_request(self, postback: dict, action: str):
-        order_id: str | None = postback.get('order_id')
-        if not order_id:
-            self.__loger.error(
-                f'''Unable to get "order_id" of "status_id" field \n
-                from postback request: \n
-                "order_id": {order_id}''')
-            return None
-        dilovod_order_id_response = await self.__dilovod_client.get_oreder_id_by_crm_id(order_id)
-        if not dilovod_order_id_response:
-            self.__loger.error(f'''Unexpected error occured while getting Dilovod order id from CRM.\n
-                                CRM "order_id": {order_id}''')
-            return None
-        try:
-            dilovod_order_id = dilovod_order_id_response[0]['id']
-        except Exception:
-            self.__loger.error(f'''Unable to get dilovod order id\n
-                                CRM order_id: {order_id}\n
-                                Response: \n{dilovod_order_id_response}''')
-            return None
-        dilovod_order_object: dict = await self.__dilovod_client.get_dilovod_order(
-            dilovod_id=dilovod_order_id)
-        if action == 'move':
-            await self.__dilovod_client.make_move(
-                dilovod_response=dilovod_order_object)
-        if action == 'shipment':
-            shipment_id: str = await self.__dilovod_client.make_shipment(
-                dilovod_response=dilovod_order_object
-            )
-            await self.__dilovod_client.make_cashIn(
-                dilovod_response=dilovod_order_object,
-                shipment_id=shipment_id
-            )
+        for oredr in postback:
+            order_id: str | None = postback.get('order_id')
+            if not order_id:
+                self.__loger.error(
+                    f'''Unable to get "order_id" of "status_id" field \n
+                    from postback request: \n
+                    "order_id": {order_id}''')
+                return None
+            dilovod_order_id_response = await self.__dilovod_client.get_oreder_id_by_crm_id(order_id)
+            if not dilovod_order_id_response:
+                self.__loger.error(f'''Unexpected error occured while getting Dilovod order id from CRM.\n
+                                    CRM "order_id": {order_id}''')
+                return None
+            try:
+                dilovod_order_id = dilovod_order_id_response[0]['id']
+            except Exception:
+                self.__loger.error(f'''Unable to get dilovod order id\n
+                                    CRM order_id: {order_id}\n
+                                    Response: \n{dilovod_order_id_response}''')
+                return None
+            dilovod_order_object: dict = await self.__dilovod_client.get_dilovod_order(
+                dilovod_id=dilovod_order_id)
+            if action == 'move':
+                await self.__dilovod_client.make_move(
+                    dilovod_response=dilovod_order_object)
+            if action == 'shipment':
+                shipment_id: str = await self.__dilovod_client.make_shipment(
+                    dilovod_response=dilovod_order_object
+                )
+                await self.__dilovod_client.make_cashIn(
+                    dilovod_response=dilovod_order_object,
+                    shipment_id=shipment_id
+                )
